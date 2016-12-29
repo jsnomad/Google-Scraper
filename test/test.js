79195dee4b54a7aa1684955dcceb829dabb93f73
Thomas Blanc-Hector <thomas.blanc.hector@gmail.com>
2016-12-29 19:06:11 +0700

    add option
    add option

--- test/test.js
+++ test/test.js
@@ -4,17 +4,17 @@ import { expect, should } from 'chai'
 const options = {
   keyword: "javascript",
   language: "fr",
-  results: 100
+  tld: "fr",
+  results: 100,
 };
 
 const scrape = new GoogleScraper(options);
 should()
-
 describe('Get google links', () => {
   it('should get at least 1 link', (done) => {
     scrape.getGoogleLinks.then((value) => {
       expect(value.length).to.be.at.least(1);
+      done()
     })
-    done()
   })
 })
